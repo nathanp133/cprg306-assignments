74e9988bb16cd2d045f883eb5a8ffd3129126914
nathanp133 <83419858+nathanp133@users.noreply.github.com>
2024-01-16 14:07:09 -0700

    first commit

---
+++ cprg306-assignments/app/week-2/student-info.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+
+    return (
+        <div>
+        <div>
+        My name is Nathan Podgurny
+        </div>
+        <Link href="">
+        Access my GitHub
+        </Link>
+        
+        </div>
+        );
+    
+}
+export default StudentInfo;

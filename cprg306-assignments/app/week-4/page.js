2141faa2e610d18bdde8068e8ba7e66798773ae8
nathanp133 <83419858+nathanp133@users.noreply.github.com>
2024-02-05 16:19:16 -0700

    added week 4
    completed

---
+++ cprg306-assignments/app/week-4/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewItem from './new-item';
+
+
+const Page = () => {
+    return (
+        <main  >
+
+            <NewItem />
+        </main>
+    );
+};
+
+export default Page;
